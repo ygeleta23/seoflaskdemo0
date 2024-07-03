fb2dc1ffdce64d52e0a24948bf1f32036e94f5e0
Yonathan Geleta <yonathan.geleta@seo-usa.org>
2024-07-03 21:04:29 +0000

    fixed path errir

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def register():
 @app.route("/update_server", methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('/home/CHANGE_TO_PYTHON_ANYWHERE_USERNAME/CHANGE_TO_GITHUB_REPO_NAME')
+        repo = git.Repo('/home/seoflaskdemo0/seoflaskdemo0')
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
